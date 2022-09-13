56d4754da51df5f480ca393127e5bc9bfc881308
Davit Gevorgyan <dgev2k@gmail.com>
2022-09-13 15:27:58 +1000

    server set up

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+require("dotenv").config();
+
+app.use(express.static("public"))
+
+const users = [{
+    username: "Andrew",
+    password: "Jackson123",
+}];
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname+'/index.html'))
+})
+
+app.get("/users", (req, res) => {
+   res.json(users); 
+})
+
+app.listen(process.env.PORT);
