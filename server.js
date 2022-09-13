8d201ea3ce4d0b61f6016afb7503e0c0909ae6fc
Davit Gevorgyan <dgev2k@gmail.com>
2022-09-13 15:43:12 +1000

    cleaner

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const users = [{
 }];
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname+'index.html'))
+    res.sendFile(path.join(__dirname))
 })
 
 app.get("/users", (req, res) => {
