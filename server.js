b76be949ddec1c2346d2fa86c330d6b4469a5cd2
Davit Gevorgyan <dgev2k@gmail.com>
2022-09-16 14:36:49 +1000

    user account creation

--- server.js
+++ server.js
@@ -1,21 +1,30 @@
 const express = require('express');
+const session = require("express-session");
+const store = new session.MemoryStore();
 const app = express();
 const path = require('path');
 require("dotenv").config();
 
-app.use(express.static("public"))
+app.use((req, res, next) => {
+    console.log(store);
+    next();
+})
 
-const users = [{
-    username: "Andrew",
-    password: "Jackson123",
-}];
+app.use(session({
+    secret: "secret",
+    cookie: { maxAge: 300000000 },
+    saveUninitialized: false,
+    store
+}))
+
+app.use(express.static("public"))
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname))
 })
 
-app.get("/users", (req, res) => {
-   res.json(users); 
-})
+app.use("/users", require('./routes/users'))
 
-app.listen(process.env.PORT);
+app.listen(process.env.PORT, async () => {
+    console.log(`Server running on port: ${process.env.PORT}`);
+});
