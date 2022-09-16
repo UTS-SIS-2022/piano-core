5f623772f4efdda5d36dd760415778f25e913c8b
Davit Gevorgyan <dgev2k@gmail.com>
2022-09-16 14:36:49 +1000

    user account creation

---
+++ routes/users.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const {
+    createUser,
+    logIn,
+    getUsers,
+    logOut
+} = require('../controllers/users');
+
+router.use(express.json());
+
+router.post('/', createUser);
+router.get('/', getUsers);
+router.get('/login', logIn);
+router.get('/logout', logOut);
+
+module.exports = router;
