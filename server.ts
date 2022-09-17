124c8d5004cd19b9fae6b0aebba6cb973d1876bf
Mike <michael.naughton@gmail.com>
2022-09-17 13:54:03 +1000

    Comment server

--- server.ts
+++ server.ts
@@ -1,57 +1,46 @@
 import { InsertOneResult, MongoClient } from "mongodb";
 import express from "express";
-// require dotenv to load environment variables
-require("dotenv").config();
+import { createComposition } from "./controllers/compositions";
 const cors = require("cors");
 const userSession = require("express-session");
 const store = new userSession.MemoryStore();
-const path = require("path");
+
+// require dotenv to load environment variables
 require("dotenv").config();
 
+// setup express
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 app.use(express.static("public"));
 
+// serve landing page
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
+// start listening
 app.listen(process.env.PORT, () =>
   console.log(`Listening on port ${process.env.PORT}!`)
 );
 
+// insert a new composition to mongodb
 app.post("/api/session", async (req, res) => {
   if (!process.env.MONGO_CONNECTION_URI) {
     console.log("MONGO_URI is not set");
-
+    res.send(500);
+    return;
+  } else {
+    const insertResult = await createComposition(req.body);
+    res.status(200).send(insertResult);
     return;
   }
-  const mongoClient = new MongoClient(process.env.MONGO_CONNECTION_URI);
-  try {
-    await mongoClient.connect();
-    const database = mongoClient.db("music");
-    const collection = database.collection("sessions");
-    const result: InsertOneResult<Document> = await collection.insertOne(
-      req.body
-    );
-    console.log(
-      `New session created with the following id: ${result.insertedId}`
-    );
-    return result.insertedId;
-  } catch (e) {
-    console.error(e);
-  }
 });
 
+// retrieve compositions from mongodb by userid
 app.get("/api/session", async (req, res) => {
   console.log("get session recieved");
-});
-
-app.use((req: any, res: any, next: () => void) => {
-  console.log(store);
-  next();
 });
 
 app.use(
@@ -64,9 +53,5 @@ app.use(
 );
 
 app.use(express.static("public"));
-
-app.get("/", (req: any, res: any) => {
-  res.sendFile(path.join(__dirname));
-});
 
 app.use("/users", require("./routes/users"));
